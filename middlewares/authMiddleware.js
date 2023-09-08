0bf2efa8a57cbad175b6407a65d225714bbf8b8c
Olosko23 <oloogeorge633@gmail.com>
2023-09-08 09:02:47 +0300

    token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -16,9 +16,13 @@ const protect = asyncHandler(async (req, res, next) => {
 
       req.user = await User.findById(decoded.userId).select("-password");
 
+      if (!req.user) {
+        res.status(401);
+        throw new Error("User not found");
+      }
+
       next();
     } catch (error) {
-      console.error(error);
       res.status(401);
       res.json("Not Authorized, token failed");
     }
