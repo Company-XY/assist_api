5edb7c9b793d32c6917c6750726079a5f51098b0
Olosko23 <oloogeorge633@gmail.com>
2023-09-11 12:07:54 +0300

    'bot'

--- controllers/botController.js
+++ controllers/botController.js
@@ -1,18 +1,19 @@
-const { NlpManager } = require("node-nlp");
 const responses = require("../models/Responses.json");
 
-const manager = new NlpManager({ languages: ["en"] });
+function findResponse(question) {
+  const foundQuestion = responses.questions.find(
+    (qna) => qna.question.toLowerCase() === question.toLowerCase()
+  );
 
-responses.questions.forEach((qna) => {
-  manager.addDocument("en", qna.question, "match", qna);
-});
-
-manager.train();
-
-// Define the getRandomResponse function
-function getRandomResponse(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+  if (foundQuestion) {
+    const randomResponse =
+      foundQuestion.response[
+        Math.floor(Math.random() * foundQuestion.response.length)
+      ];
+    return randomResponse;
+  } else {
+    return "I'm sorry, I don't have a response to that question. Would you like to talk to one of our agents";
+  }
 }
 
 function handleUserMessage(req, res) {
@@ -22,29 +23,15 @@ function handleUserMessage(req, res) {
     return res.status(400).json({ error: "Message is required." });
   }
 
-  manager
-    .process("en", message)
-    .then((result) => {
-      const bestMatch = result.score > 0.7 ? result.intent : null;
+  const response = findResponse(message);
 
-      if (bestMatch) {
-        const matchedQuestion = bestMatch.metadata;
-        const response = getRandomResponse(matchedQuestion.response);
-        console.log("Matched question:", matchedQuestion.question);
-        console.log("Bot response:", response);
-        return res.json({ response });
-      }
-
-      const defaultResponse = "I'm sorry, I don't understand that question.";
-      console.log("No matching question found for:", message);
-      res.json({ response: defaultResponse });
-    })
-    .catch((error) => {
-      console.error(error);
-      const defaultResponse =
-        "I'm sorry, an error occurred while processing your request.";
-      res.json({ response: defaultResponse });
-    });
+  if (response) {
+    return res.json({ response });
+  } else {
+    const defaultResponse =
+      "I'm sorry, I don't understand that question. Would you like to talk to one of our agents";
+    return res.json({ response: defaultResponse });
+  }
 }
 
 module.exports = { handleUserMessage };
