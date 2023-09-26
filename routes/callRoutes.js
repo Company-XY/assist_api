e59503268ebdb0fdf480683c83378ff07b851bba
Olosko23 <oloogeorge633@gmail.com>
2023-09-26 09:52:41 +0300

    'edit'

--- routes/callRoutes.js
+++ routes/callRoutes.js
@@ -10,19 +10,14 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Get all calls
 router.get("/calls", protect, getAllCalls);
 
-// Create a new call
 router.post("/calls", protect, createCall);
 
-// Get a single call by ID
 router.get("/calls/:id", protect, getOneCall);
 
-// Update a call by ID
-router.put("/calls/:id", protect, updateCall);
+router.patch("/calls/:id", protect, updateCall);
 
-// Delete a call by ID
 router.delete("/calls/:id", protect, deleteCall);
 
 module.exports = router;
--- routes/detailsRoutes.js
+++ routes/detailsRoutes.js
@@ -10,19 +10,14 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Get all details
 router.get("/details", protect, getAllDetails);
 
-// Create a new detail
 router.post("/details", protect, createDetail);
 
-// Get a single detail by ID
 router.get("/details/:id", protect, getOneDetail);
 
-// Update a detail by ID
-router.put("/details/:id", protect, updateDetail);
+router.patch("/details/:id", protect, updateDetail);
 
-// Delete a detail by ID
 router.delete("/details/:id", protect, deleteDetail);
 
 module.exports = router;
