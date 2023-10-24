7ff12bf01420673ed4d41e490f8b49dec8c4f908
Olosko23 <oloogeorge633@gmail.com>
2023-10-24 13:28:09 +0300

    'profile'

--- controllers/userController.js
+++ controllers/userController.js
@@ -320,7 +320,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     const { id } = req.params;
     const updateFields = req.body;
 
-    const user = await User.findById(id);
+    const user = await User.findByIdAndUpdate(id, updateFields, { new: true });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -336,30 +336,22 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       }
     }
 
-    upload.single("avatar")(req, res, async (err) => {
-      if (err) {
-        return res.status(400).json({ message: "Error uploading file" });
-      }
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path);
 
-      if (req.file) {
-        const result = await cloudinary.uploader.upload(req.file.path);
+      user.avatar = {
+        title: req.file.originalname,
+        imageUrl: result.secure_url,
+      };
+    }
 
-        user.avatar = {
-          title: req.file.originalname,
-          imageUrl: result.secure_url,
-        };
-      }
+    await user.save();
 
-      for (const key in updateFields) {
-        user[key] = updateFields[key];
-      }
-
-      const updatedUser = await user.save();
-
-      res.status(200).json(updatedUser);
-    });
+    res.status(200).json({ message: "User profile updated", user });
   } catch (error) {
-    res.status(400).json(error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
     console.error(error);
   }
 });
